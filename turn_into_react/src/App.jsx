c5890a185416572b0060e7e8bfd57f51b14ac9c6
Parth <parsjoshi2@gmail.com>
2025-05-16 13:14:29 +0530

    React website updated

---
+++ turn_into_react/src/App.jsx
@@ -0,0 +1,20 @@
+// import * as gsap from "./js/index";
+// import * as imageLoad from "./js/imagesloaded";
+// import * as indexJs from "./js/index";
+// import * as lenis from "./js/lenis";
+// import * as smoothScroll from "./js/smoothscroll";
+// import * as utils from "./js/utils";
+
+
+function App() {
+  
+
+  return (
+    <>
+
+
+    </>
+  )
+}
+
+export default App
